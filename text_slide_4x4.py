20df0c8fcd0363faa220ca4729f872622156871c
gabyru12 <164394847+gabyru12@users.noreply.github.com>
2024-03-26 13:27:54 +0000

    Update text_slide_4x4.py

--- text_slide_4x4.py
+++ text_slide_4x4.py
@@ -11,17 +11,24 @@ font = pygame.font.SysFont(None,40)
 
 def game():
     run = True
+    clock = pygame.time.Clock()
+    turn = True
+
     game_screen_rect = pygame.Rect(width/2-400/2,250,400,400)
     CELL_SIZE = 100
     GRID_SIZE = 4
-    clock = pygame.time.Clock()
+
     red_rect = pygame.Rect((width/2-400/2,250,100,100))
     speed = 1
     movement_left,movement_right,movement_up,movement_down = False,False,False,False
-    direction = ["left","right","up","down"]
+
     while run:
         screen.fill("light blue")
         clock.tick(1800)
+        
+        if movement_left==movement_right==movement_up==movement_down==False:
+            turn = True
+
         game_screen = pygame.draw.rect(screen,"white",game_screen_rect,0,0)
         red = pygame.draw.rect(screen,"red",red_rect)
         pygame.draw.rect(screen,"white",red_rect,5)
@@ -30,18 +37,23 @@ def game():
                     run = False
                     quit()
                     exit()
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_ESCAPE:
-                        run = False
-                        main_menu()
-                    if event.key == pygame.K_a:
-                        movement_left = True
-                    if event.key == pygame.K_d:
-                        movement_right = True
-                    if event.key == pygame.K_w:
-                        movement_up = True
-                    if event.key == pygame.K_s:
-                        movement_down = True
+                if turn:
+                    if event.type == pygame.KEYDOWN:
+                        if event.key == pygame.K_ESCAPE:
+                            run = False
+                            main_menu()
+                        if event.key == pygame.K_a:
+                            movement_left = True
+                            turn = False
+                        if event.key == pygame.K_d:
+                            movement_right = True
+                            turn = False
+                        if event.key == pygame.K_w:
+                            movement_up = True
+                            turn = False
+                        if event.key == pygame.K_s:
+                            movement_down = True
+                            turn = False
         #horizontal lines
         for i in range(1, GRID_SIZE):
             pygame.draw.line(screen, "black", (50, 250+i * CELL_SIZE), (width-50, 250+i * CELL_SIZE))
@@ -57,7 +69,7 @@ def game():
             movement_up = False
         if red_rect.y+100+speed > game_screen_rect.y+400:
             movement_down = False
-        #se A e D ou W e S forem clicadas ele vai ficar num loop 1-1 = 0 e ficar parado horizontal ou verticalmente
+            
         if movement_left == True: 
             red_rect.x -= speed
         if movement_right == True: 
