cf905cced0c8aea0f7a45f06b92ce675c9710542
gabyru12 <164394847+gabyru12@users.noreply.github.com>
2024-04-12 16:07:01 +0100

    Update piece_choice.py

--- piece_choice.py
+++ piece_choice.py
@@ -20,9 +20,9 @@ def play():
         click = False
         mx,my = pygame.mouse.get_pos()
         screen.fill("light blue")
-        play_width,play_height = 120,60
-        play_rec = pygame.draw.rect(screen,"grey",(WIDTH/2-play_width//2,HEIGHT/2-play_height//2,play_width,play_height), 0)
-        play_border = pygame.draw.rect(screen,"black",(WIDTH/2-play_width//2,HEIGHT/2-play_height//2,play_width,play_height), 5)
+        rec_width,rec_height = 120,60
+        play_rec = pygame.draw.rect(screen,"grey",(WIDTH/2-rec_width//2,HEIGHT/2-rec_height//2,rec_width,rec_height), 0)
+        play_border = pygame.draw.rect(screen,"black",(WIDTH/2-rec_width//2,HEIGHT/2-rec_height//2,rec_width,rec_height), 5)
         play = font.render("Play",1,"black")
         screen.blit(play, (WIDTH/2 - play.get_width()/2,HEIGHT/2 - play.get_height()/2))
 
@@ -35,7 +35,35 @@ def play():
                 click = True
 
         if play_rec.collidepoint(mx,my):
-            play_border = pygame.draw.rect(screen,"yellow",(WIDTH/2-play_width//2,HEIGHT/2-play_height//2,play_width,play_height), 5)
+            play_border = pygame.draw.rect(screen,"yellow",(WIDTH/2-rec_width//2,HEIGHT/2-rec_height//2,rec_width,rec_height), 5)
+            if click == True:
+                run = False
+                main_menu()
+
+        pygame.display.update()
+
+def main_menu():
+    run = True
+    while run:
+        click = False
+        mx,my = pygame.mouse.get_pos()
+        screen.fill("light blue")
+        rec_width,rec_height = 270,60
+        board_creator_rec = pygame.draw.rect(screen,"grey",(WIDTH/2-rec_width//2,HEIGHT/3-rec_height//2,rec_width,rec_height), 0)
+        board_creator_border = pygame.draw.rect(screen,"black",(WIDTH/2-rec_width//2,HEIGHT/3-rec_height//2,rec_width,rec_height), 5)
+        board_creator = font.render("Create your board",1,"black")
+        screen.blit(board_creator, (WIDTH/2 - board_creator.get_width()/2,HEIGHT/3 - board_creator.get_height()/2))
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                quit()
+                exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                click = True
+
+        if board_creator_rec.collidepoint(mx,my):
+            board_creator_border = pygame.draw.rect(screen,"yellow",(WIDTH/2-rec_width//2,HEIGHT/3-rec_height//2,rec_width,rec_height), 5)
             if click == True:
                 run = False
                 choose_board(selected_size)
@@ -68,12 +96,12 @@ def draw_board(size,board,mx,my):
 
     for row in range(len(board)):
         for col in range(len(board)):
-            if board[row][col][0] == "1":
+            if board[row][col][0:2] == "1r":
                 pygame.draw.rect(screen, "red", ((margin_x + col * (square_size + 2)), (margin_y + row * (square_size + 2)), square_size, square_size))
-            if board[row][col][0] == "2":
-                pygame.draw.rect(screen, "yellow", ((margin_x + col * (square_size + 2)), (margin_y + row * (square_size + 2)), square_size, square_size))
-            if board[row][col] == "3":
-                pygame.draw.rect(screen, "black", ((margin_x + col * (square_size + 2)), (margin_y + row * (square_size + 2)), square_size, square_size))
+            if board[row][col][0:2] == "1g":
+                pygame.draw.rect(screen, "green", ((margin_x + col * (square_size + 2)), (margin_y + row * (square_size + 2)), square_size, square_size))
+            if board[row][col][0:2] == "1b":
+                pygame.draw.rect(screen, "blue", ((margin_x + col * (square_size + 2)), (margin_y + row * (square_size + 2)), square_size, square_size))
 
     pygame.draw.rect(screen, "black", (margin_x, margin_y, board_size, board_size),2)
 
@@ -145,6 +173,10 @@ def choose_board(selected_size):
                 run = False
                 quit()
                 exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    run = False
+                    main_menu()
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 click = True
 
@@ -165,40 +197,61 @@ def choose_board(selected_size):
 
         pygame.display.update()
 
+def draw_pieces():
+    title_text = font.render("Posicione as peças:", True, "black")
+    screen.blit(title_text, (WIDTH // 2 - title_text.get_width() // 2, 50))
+
+    button_width, button_height = 50, 50
+    button_y = 150
+    button_x_start = (WIDTH - 3 * button_width - 2 * 50) // 2
+
+    button_piece_red = pygame.Rect(button_x_start, button_y, button_width, button_height)
+    pygame.draw.rect(screen, "red", button_piece_red)
+
+    button_piece_green = pygame.Rect(button_x_start + button_width + 50, button_y, button_width, button_height)
+    pygame.draw.rect(screen, "green", button_piece_green)
+
+    button_piece_blue = pygame.Rect(button_x_start + 2 * (button_width + 50), button_y, button_width, button_height)
+    pygame.draw.rect(screen, "blue", button_piece_blue)
+
+    return button_x_start, button_y, button_width, button_height
+
+#def draw_finish():
+
 def put_pieces(selected_size):
     run = True
     selected_piece = None
     board_lenght = int(selected_size[0])
     board = [["0" for _ in range(board_lenght)]for _ in range(board_lenght)]
     piece_color = None
+    draw_piece_flag = True
+    draw_finish_flag = False
+    counter = 0
+    counter_red = 0
+    counter_green = 0
+    counter_blue = 0
     while run:
         screen.fill("light blue")  # light blue color
         
         mx,my = pygame.mouse.get_pos()
 
-        # Texto do título
-        title_text = font.render("Posicione as peças:", True, "black")
-        screen.blit(title_text, (WIDTH // 2 - title_text.get_width() // 2, 50))
 
-        # Botões para escolher o tipo de peça
-        button_width, button_height = 150, 50
-        button_y = 150
-        button_x_start = (WIDTH - 3 * button_width - 2 * 50) // 2
+        if draw_piece_flag:
+            button_x_start, button_y, button_width, button_height = draw_pieces()
 
-        button_piece1 = pygame.Rect(button_x_start, button_y, button_width, button_height)
-        pygame.draw.rect(screen, "red", button_piece1)
-        text_piece1 = font.render("Peça 1", True, "black")
-        screen.blit(text_piece1, (button_x_start + button_width // 2 - text_piece1.get_width() // 2, button_y + button_height // 2 - text_piece1.get_height() // 2))
+        if counter == 3:
+            selected_piece = None
+            pygame.draw.line(screen, "black", (button_x_start, button_y), (button_x_start + button_width, button_y + button_height), 5)
+            pygame.draw.line(screen, "black", (button_x_start, button_y + button_height), (button_x_start + button_width, button_y), 5)
 
-        button_piece2 = pygame.Rect(button_x_start + button_width + 50, button_y, button_width, button_height)
-        pygame.draw.rect(screen, "yellow", button_piece2)
-        text_piece2 = font.render("Peça 2", True, "black")
-        screen.blit(text_piece2, (button_x_start + button_width + 50 + button_width // 2 - text_piece2.get_width() // 2, button_y + button_height // 2 - text_piece2.get_height() // 2))
+            pygame.draw.line(screen, "black", (button_x_start + button_width + 50, button_y), (button_x_start + button_width + 50 + button_width, button_y + button_height), 5)
+            pygame.draw.line(screen, "black", (button_x_start + button_width + 50, button_y + button_height), (button_x_start + button_width + 50 + button_width, button_y), 5)
 
-        button_piece3 = pygame.Rect(button_x_start + 2 * (button_width + 50), button_y, button_width, button_height)
-        pygame.draw.rect(screen, "black", button_piece3)
-        text_piece3 = font.render("Peça 3", True, "white")
-        screen.blit(text_piece3, (button_x_start + 2 * (button_width + 50) + button_width // 2 - text_piece3.get_width() // 2, button_y + button_height // 2 - text_piece3.get_height() // 2))
+            pygame.draw.line(screen, "black", (button_x_start + 2 * (button_width + 50), button_y), (button_x_start + 2 * (button_width + 50) + button_width, button_y + button_height), 5)
+            pygame.draw.line(screen, "black", (button_x_start + 2 * (button_width + 50), button_y + button_height), (button_x_start + 2 * (button_width + 50) + button_width, button_y), 5)
+            
+        if draw_finish_flag:
+            draw_finish()
 
         click = False
 
@@ -207,21 +260,20 @@ def put_pieces(selected_size):
                 run = False
                 quit()
                 exit()
-            if event.type == pygame.KEYDOWN:
+            elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     run = False
-                    choose_board()
+                    choose_board(selected_size)
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 click = True
 
         if click:
-            button_x_start = (WIDTH - 3 * 150 - 2 * 50) // 2
-            if button_x_start <= mx <= button_x_start + 150 and 150 <= my <= 200:
-                selected_piece = "Peça 1"
-            elif button_x_start + 200 <= mx <= button_x_start + 350 and 150 <= my <= 200:
-                selected_piece = "Peça 2"
-            elif button_x_start + 400 <= mx <= button_x_start + 550 and 150 <= my <= 200:
-                selected_piece = "Peça 3"
+            if button_x_start <= mx <= button_x_start + button_width and button_y <= my <= button_y + button_height:
+                selected_piece = "RED"
+            elif button_x_start + button_width + 50 <= mx <= button_x_start + button_width + 50 + button_width and button_y <= my <= button_y + button_height:
+                selected_piece = "GREEN"
+            elif button_x_start + 2 * (button_width + 50) <= mx <= button_x_start + 2 * (button_width + 50) + button_width and button_y <= my <= button_y + button_height:
+                selected_piece = "BLUE"
 
         margin_x, margin_y, square_size, rows, cols, board_size = draw_board(selected_size,board,mx,my)
 
@@ -229,23 +281,31 @@ def put_pieces(selected_size):
             col_clicked = int((mx - margin_x) // square_size)
             row_clicked = int((my - margin_y) // square_size)
             if 0 <= col_clicked < cols and 0 <= row_clicked < rows:
-                if selected_piece == "Peça 1":
+                if selected_piece == "RED":
                     piece_color = "red"
-                elif selected_piece == "Peça 2":
-                    piece_color = "yellow"
-                elif selected_piece == "Peça 3":
-                    piece_color = "black"
+                elif selected_piece == "GREEN":
+                    piece_color = "green"
+                elif selected_piece == "BLUE":
+                    piece_color = "blue"
                 draw_piece(screen, square_size, margin_x, margin_y, col_clicked, row_clicked, piece_color,selected_size)
             if click:
                 if margin_x <= mx <= margin_x + board_size and margin_y <= my <= margin_y + board_size:
                     if piece_color == "red":
-                        board[row_clicked][col_clicked] = "1"
-                    if piece_color == "yellow":
-                        board[row_clicked][col_clicked] = "2"
-                    if piece_color == "black":
-                        board[row_clicked][col_clicked] = "3"
+                        counter += 1
+                        counter_red += 1
+                        board[row_clicked][col_clicked] = "1r" 
+                    if piece_color == "green":
+                        counter += 1
+                        counter_green += 1
+                        board[row_clicked][col_clicked] = "1g"
+                    if piece_color == "blue":
+                        counter += 1
+                        counter_blue += 1
+                        board[row_clicked][col_clicked] = "1b"
 
-    
+        counter_text = font.render(f"{counter}",True,"black")
+        screen.blit(counter_text, (WIDTH - 50,50))
+
         pygame.display.update()
 
 play()
