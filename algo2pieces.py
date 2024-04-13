706b1f7ffddbada1896a126bf6d61f64893099ce
gabyru12 <164394847+gabyru12@users.noreply.github.com>
2024-04-13 02:15:00 +0100

    Update algo2pieces.py

--- algo2pieces.py
+++ algo2pieces.py
@@ -1,5 +1,6 @@
 import copy
 # Cria tabuleiro
+#puzzle 47 Normal
 board = [
     ["3","3","0","3"],
     ["0","3","0","1r1"],
@@ -9,7 +10,6 @@ board = [
 
 possible_pieces = ["1r1","1g1","1b1","1r2","1g2","1b2","1r3","1g3","1b3"]
 respective_finish = ["2r1","2g1","2b1","2r2","2g2","2b2","2r3","2g3","2b3"]
-
 # lista das peças usadas                
 def used_pieces(board):
     pieces = []
@@ -30,10 +30,10 @@ def list_finish(board):
 # Dá a posição de uma peça
 def pos_pieces(boardstate):
     pos_pieces = {}
-    for i in range(len(board)):
-        for j in range(len(board[i])):
-            if board[i][j][0] == "1":
-                pos_pieces[board[i][j]] = (i,j)
+    for i in range(len(boardstate)):
+        for j in range(len(boardstate)):
+            if boardstate[i][j][0] == "1":
+                pos_pieces[boardstate[i][j]] = (i,j)
     return pos_pieces
 
 def pos_finish(board):
@@ -58,33 +58,34 @@ def pos_finish(board):
     return 0
 
 # Verifica se soução já foi encontrada
-def solution_check(used_pieces,list_finish,pos_pieces,pos_finish,new_board,board):
-    global dic,dic1
-    pieces = used_pieces(new_board)
-    finishes = list_finish(board)
+def solution_check(pieces,winning_points,pos_pieces,pos_finish):
     for i in range(len(pieces)):
         key = pieces[i]
-        key1 = finishes[i]
-        print(i)
-        if pos_pieces(new_board)[key] != pos_finish(board)[key1]:
-            return "false"
-    return "true"
+        key1 = winning_points[i]
+        if pos_pieces[key] != pos_finish[key1]:
+            return False
+    return True
+
+def update_finish(new_board,board):
+    for row in range(len(board)):
+        for col in range(len(board)):
+            if board[row][col][0] == "2" and new_board[row][col] == "0":
+                    new_board[row][col] = board[row][col]
 
 # Dá os movimentos possíveis
-def check_move(pos_pieces,used_pieces,new_board,board):
+def check_move(pos_pieces,pieces,new_board,board):
     moves_bool = {"move_up": False, "move_down": False, "move_left": False, "move_right": False}
-    pos_pieces = pos_pieces(new_board) #posição das peças usadas
-    pieces = used_pieces(board) #lista das peças usadas
+    piece_positions = pos_pieces(new_board) #posição das peças usadas
     moves = [] #movimentos possíveis
     for i in range(len(pieces)):
-        row,col = pos_pieces[pieces[i]]
-        if col != 0 and board[row][col-1] != "3":
+        row,col = piece_positions[pieces[i]]
+        if col-1 > -1 and board[row][col-1] != "3":
                 moves_bool["move_left"] = True
-        if col != len(board)-1 and board[row][col+1] != "3":
+        if col+1 < len(board) and board[row][col+1] != "3":
                 moves_bool["move_right"] = True
-        if row != 0 and board[row-1][col] != "3":
+        if row-1 > -1 and board[row-1][col] != "3":
                 moves_bool["move_up"] = True
-        if row != len(board)-1 and board[row+1][col] != "3":
+        if row+1 < len(board) and board[row+1][col] != "3":
                 moves_bool["move_down"] = True
     for key,value in moves_bool.items():
         if value:
@@ -101,91 +102,86 @@ counter = 0
 #daqui pa baixo preciso acrescentar cenas
 
 # Faz o movimento e retorna novos estados para memo1 
-def do_move(check_move, pos_piece,board):
+def do_move(used_pieces,list_finish,pos_finish,check_move, pos_piece,board,solution_check):
     global memo,memo1,counter
     flag = False
+    pieces = used_pieces(board)
+    finish = list_finish(board)
+    finish_position = pos_finish(board)
     if len(memo1) != 0:    
         memo = copy.deepcopy(memo1)
         memo1 = []
     for i in range(len(memo)):
         if flag:
             break
-        piece_positions = pos_pieces(memo[i])
-        moves = check_move(pos_piece,used_pieces,memo[i],board)
+        moves = check_move(pos_pieces,pieces,memo[i],board)
         for j in range(len(moves)):
-            pieces = used_pieces(board)
             new_board = copy.deepcopy(memo[i])
             if moves[j] == "move_up":
-                while len(pieces) != 0:
-                    for key in range(len(pieces)):
-                        for row in new_board:
-                            print("  ".join(row))
-                        print("\n")
-                        print(moves)
-                        piece = pieces[key]
-                        ori_x,ori_y = piece_positions[piece]
-                        x,y = pos_pieces(new_board)[piece]
-                        if x-1 != -1 and new_board[x-1][y] != "3" and new_board[x-1][y][0] != "1": #se tiverem 2 na mesma coluna isto dá errado
-                            new_board[x-1][y] = key
-                            new_board[x][y] = "0"
-                        else:
-                            pieces.remove(piece)    
+                for k in range(len(new_board)):
+                    for row in range(len(new_board)):
+                        for col in range(len(new_board)):
+                            if new_board[row][col][0] == "1":
+                                if row-1 > -1 and new_board[row-1][col] != "3" and new_board[row-1][col][0] != "1": #se tiverem 2 na mesma coluna isto dá errado
+                                    new_board[row-1][col] = new_board[row][col]
+                                    new_board[row][col] = "0"
+                    update_finish(new_board,board)    
                 if new_board in visited:
                     continue
                 else:
                     visited.append(new_board)
                 memo1.append(new_board)
+                pieces = used_pieces(new_board)
+                piece_postition = pos_pieces(new_board)
             elif moves[j] == "move_down":
-                pieces.reverse()           
-                while len(pieces) != 0:
-                    for piece in range(len(pieces)):
-                        key = pieces[key]
-                        ori_x,ori_y = piece_positions[key]
-                        x,y = pos_pieces(new_board)[key]
-                        if x+1 != len(new_board) and new_board[x+1][y] != "3" and new_board[x+1][y][0] != "1": #se tiverem 2 na mesma coluna isto dá errado
-                            new_board[x+1][y] = key
-                            new_board[x][y] = "0"
-                        else:
-                            pieces.remove(key)    
+                for k in range(len(new_board)):
+                    for row in range(len(new_board)):
+                        for col in range(len(new_board)):
+                            if new_board[row][col][0] == "1":
+                                if row+1 < len(board) and new_board[row+1][col] != "3" and new_board[row+1][col][0] != "1": #se tiverem 2 na mesma coluna isto dá errado
+                                    new_board[row+1][col] = new_board[row][col]
+                                    new_board[row][col] = "0" 
+                    update_finish(new_board,board)   
                 if new_board in visited:
                     continue
                 else:
                     visited.append(new_board)
                 memo1.append(new_board)
+                pieces = used_pieces(new_board)
+                piece_postition = pos_pieces(new_board)
             elif moves[j] == "move_left":  
-                while len(pieces) != 0:
-                    for piece in range(len(pieces)):
-                        key = pieces[key]
-                        ori_x,ori_y = piece_positions[key]
-                        x,y = pos_pieces(new_board)[key]
-                        if y-1 != -1 and new_board[x][y-1] != "3" and new_board[x][y-1][0] != "1": #se tiverem 2 na mesma coluna isto dá errado
-                            new_board[x][y-1] = key
-                            new_board[x][y] = "0"
-                        else:
-                            pieces.remove(key)    
+                for k in range(len(new_board)):
+                    for row in range(len(new_board)):
+                        for col in range(len(new_board)):
+                            if new_board[row][col][0] == "1":
+                                if col-1 > -1 and new_board[row][col-1] != "3" and new_board[row][col-1][0] != "1":
+                                    new_board[row][col-1] = new_board[row][col]
+                                    new_board[row][col] = "0"
+                    update_finish(new_board,board)    
                 if new_board in visited:
                     continue
                 else:
                     visited.append(new_board)
                 memo1.append(new_board)
+                pieces = used_pieces(new_board)
+                piece_postition = pos_pieces(new_board)
             elif moves[j] == "move_right":
-                pieces.reverse()  
-                while len(pieces) != 0:
-                    for piece in range(len(pieces)):
-                        key = pieces[key]
-                        ori_x,ori_y = piece_positions[key]
-                        x,y = pos_pieces(new_board)[key]
-                        if y+1 != len(new_board) and new_board[x][y+1] != "3" and new_board[x][y+1][0] != "1": #se tiverem 2 na mesma coluna isto dá errado
-                            new_board[x][y+1] = key
-                            new_board[x][y] = "0"
-                        else:
-                            pieces.remove(key)    
+                for k in range(len(new_board)):
+                    for row in range(len(new_board)):
+                        for col in range(len(new_board)):
+                            if new_board[row][col][0] == "1":
+                                if col+1 < len(board) and new_board[row][col+1] != "3" and new_board[row][col+1][0] != "1": 
+                                    new_board[row][col+1] = new_board[row][col]
+                                    new_board[row][col] = "0"    
+                    update_finish(new_board,board)    
                 if new_board in visited:
                     continue
                 else:
                     visited.append(new_board)
                 memo1.append(new_board)
-            if solution_check(used_pieces,list_finish,pos_pieces,pos_finish,new_board,board): 
+                pieces = used_pieces(new_board)
+                piece_postition = pos_pieces(new_board)
+            if solution_check(pieces,finish,piece_postition,finish_position):
                 flag = True
                 print("Solução encontrada\n")
                 break
@@ -204,9 +200,12 @@ def print_board(memo):
 
 print("Initial Board:")
 print_board(memo)
-
-print(f"counter: {counter}\n")
-do_move(check_move,pos_pieces,board)
+do_move(used_pieces,list_finish,pos_finish,check_move,pos_pieces,board,solution_check)
+do_move(used_pieces,list_finish,pos_finish,check_move,pos_pieces,board,solution_check)
+do_move(used_pieces,list_finish,pos_finish,check_move,pos_pieces,board,solution_check)
+do_move(used_pieces,list_finish,pos_finish,check_move,pos_pieces,board,solution_check)
+do_move(used_pieces,list_finish,pos_finish,check_move,pos_pieces,board,solution_check)
+do_move(used_pieces,list_finish,pos_finish,check_move,pos_pieces,board,solution_check)
+do_move(used_pieces,list_finish,pos_finish,check_move,pos_pieces,board,solution_check)
+do_move(used_pieces,list_finish,pos_finish,check_move,pos_pieces,board,solution_check)
 print_board(memo1)
-
-
